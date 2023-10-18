32604ab9fae6c15d78cf41a6ae35649fe4fda37b
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 14:16:57 +0800

    feat: update node-caniuse-lite to 1.0.30001547+dfsg+_1.0.2

--- commit.js
+++ commit.js
@@ -1,6 +1,7 @@
-const { execSync } = require('child_process')
+const { execSync } = require('node:child_process')
 
 const pkg = require('./package.json')
 
-execSync('git add data/ package.json yarn.lock')
+execSync('git add data/ package.json pnpm-lock.yaml')
 execSync(`git commit -m "Update caniuse-db ${pkg.version}"`)
+execSync(`git tag -m "Release caniuse-db ${pkg.version}" ${pkg.version}`)
--- src/packer/feature.js
+++ src/packer/feature.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const fs = require('fs').promises
+const path = require('node:path')
+const fs = require('node:fs').promises
 const t = require('@babel/types')
-const R = require('ramda')
 
 const generateCode = require('../lib/generateCode')
 const getContentsFactory = require('../lib/getContents')
@@ -9,16 +8,16 @@ const moduleExports = require('../lib/moduleExports')
 const stringifyObject = require('../lib/stringifyObject')
 const statuses = require('../../dist/lib/statuses')
 const supported = require('../../dist/lib/supported')
+const fromEntries = require('../util/fromEntries')
+const invertObj = require('../util/invertObj')
 const parseDecimal = require('../util/parseDecimal')
-const pow = require('../util/pow')
+const sum = require('../util/sum')
 const browsers = require('../../data/browsers')
 const versions = require('../../data/browserVersions')
 
-const browsersInverted = R.invertObj(browsers)
-const statusesInverted = R.invertObj(statuses)
-const versionsInverted = R.invertObj(versions)
-
-const objFromKeys = R.curry((fn, keys) => R.zipObj(keys, R.map(fn, keys)))
+const browsersInverted = invertObj(browsers)
+const statusesInverted = invertObj(statuses)
+const versionsInverted = invertObj(versions)
 
 const base = path.join(
   path.dirname(require.resolve(`caniuse-db/data.json`)),
@@ -27,89 +26,73 @@ const base = path.join(
 
 const getContents = getContentsFactory(base)
 
-const callExpression = R.curryN(2, t.callExpression)
+const requireCall = moduleName =>
+  t.callExpression(t.identifier('require'), [t.stringLiteral(moduleName)])
 
-const requireCall = R.compose(
-  callExpression(t.identifier('require')),
-  R.of,
-  t.stringLiteral
-)
-
-const featureIndex = R.compose(
-  generateCode,
-  t.program,
-  R.of,
-  moduleExports,
-  t.objectExpression,
-  R.values,
-  R.mapObjIndexed((value, key) =>
-    t.objectProperty(t.stringLiteral(key), requireCall(value))
-  ),
-  objFromKeys(R.concat('./features/')),
-  R.map(R.prop('name'))
-)
-
-const packSupport = R.compose(
-  R.sum,
-  R.map(
-    R.ifElse(
-      R.flip(R.has)(supported),
-      R.flip(R.prop)(supported),
-      R.compose(pow(2), R.add(6), parseDecimal, R.slice(1, Infinity))
+function featureIndex(features) {
+  let index = t.objectExpression(
+    features.map(({ name }) =>
+      t.objectProperty(t.stringLiteral(name), requireCall(`./features/${name}`))
     )
-  ),
-  R.split(' ')
-)
+  )
 
-module.exports = function packFeature() {
-  return fs
-    .readdir(base)
-    .then(getContents)
-    .then(
-      R.tap(features =>
-        Promise.all(
-          features.map(feature => {
-            let { name, contents } = feature
-            let packed = {}
-            packed.A = Object.keys(contents.stats).reduce(
-              (browserStats, key) => {
-                let browser = contents.stats[key]
-                let supportData = Object.keys(browser).reduce(
-                  (stats, version) => {
-                    let support = browser[version]
-                    stats[versionsInverted[version]] = packSupport(support)
-                    return stats
-                  },
-                  {}
-                )
-                let compacted = Object.keys(supportData).reduce((min, k) => {
-                  let value = supportData[k]
-                  if (!min[value]) {
-                    min[value] = k
-                  } else {
-                    min[value] += ` ${k}`
-                  }
-                  return min
-                }, {})
-                browserStats[browsersInverted[key]] = compacted
-                return browserStats
-              },
-              {}
-            )
-            packed.B = parseDecimal(statusesInverted[contents.status])
-            packed.C = contents.title
-            return fs.writeFile(
-              path.join(__dirname, `../../data/features/${name}.js`),
-              stringifyObject(packed)
-            )
-          })
-        )
+  return generateCode([moduleExports(index)])
+}
+
+function packSupport(supportData) {
+  return sum(
+    supportData.split(' ').map(support => {
+      if (support in supported) {
+        return supported[support]
+      }
+      return 2 ** (6 + parseDecimal(support.slice(1)))
+    })
+  )
+}
+
+module.exports = async function packFeature() {
+  let features = await fs.readdir(base).then(getContents)
+
+  await Promise.all(
+    features.map(({ name, contents }) => {
+      let packed = {}
+
+      packed.A = fromEntries(
+        Object.entries(contents.stats).map(([key, browser]) => {
+          let supportData = fromEntries(
+            Object.entries(browser).map(([version, support]) => [
+              versionsInverted[version],
+              packSupport(support)
+            ])
+          )
+
+          let compacted = Object.entries(supportData).reduce(
+            (min, [k, value]) => {
+              if (!min[value]) {
+                min[value] = k
+              } else {
+                min[value] += ` ${k}`
+              }
+              return min
+            },
+            {}
+          )
+
+          return [browsersInverted[key], compacted]
+        })
       )
-    )
-    .then(features =>
-      fs.writeFile(
-        path.join(__dirname, '../../data/features.js'),
-        featureIndex(features)
+      packed.B = parseDecimal(statusesInverted[contents.status])
+      packed.C = contents.title
+      packed.D = contents.shown
+      return fs.writeFile(
+        path.join(__dirname, `../../data/features/${name}.js`),
+        stringifyObject(packed)
       )
-    )
+    })
+  )
+
+  return fs.writeFile(
+    path.join(__dirname, '../../data/features.js'),
+    featureIndex(features)
+  )
 }
